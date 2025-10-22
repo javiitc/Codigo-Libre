1da7515e5dacfece76985b9218d38b90504460c9
Javi <itsjavii1610@gmail.com>
2025-10-22 10:41:59 +0200

    Codigo

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -3,7 +3,22 @@ import java.util.Scanner;
 public class Main {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        System.out.print("Te damos la bienvenida a esta ronda de preguntas rápidas. ");
+        System.out.println("Constará de 5 preguntas relacionadas al tema que elijas. Ahora, selecciona el tema de las preguntas:");
+        System.out.println("1: Videojuegos");
+        System.out.println("2: Peliculas");
+        System.out.println("3: Cultura general");
+        System.out.println("4: Musica");
+        int seleccion = sc.nextInt();
+
+        if ((seleccion > 4) || (seleccion < 1)){
+            System.out.println("Selección no válida, intentelo de nuevo.");
+        }
+
+        if (seleccion == 1) {
+
+        }
+
 
     }
 }
