8b3536b5a846db5f6e0dc1306552f32ed971feec
Javi <itsjavii1610@gmail.com>
2025-11-04 20:09:07 +0100

    Codigo

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class Main {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         System.out.print("Te damos la bienvenida a esta ronda de preguntas rápidas. ");
         System.out.println("Constará de 3 preguntas relacionadas al tema que elijas. Ahora, selecciona el tema de las preguntas:");
         System.out.println("1: Videojuegos");
         System.out.println("2: Peliculas");
+
         int seleccion = sc.nextInt();
         int respuesta = 0;
         int respuesta2 = 0;
@@ -20,6 +22,7 @@ public class Main {
                 break;
             }
         }
+
 
         if (seleccion == 1) {
             System.out.println("Has seleccionado el tema 'Videjouegos'. Serán 3 preguntas, en el orden que elijas. Sabrás la pregunta cuando la selecciones.");
@@ -41,7 +44,9 @@ public class Main {
                 } else {
                     System.out.println("Incorrecto, seleccione la siguiente pregunta");
                 }
+
                 pregunta2 = sc.nextInt();
+
                 while (pregunta2 == 1) {
                     System.out.println("Pregunta ya seleccionada, por favor seleccione otra");
                     pregunta2 = sc.nextInt();
@@ -83,9 +88,9 @@ public class Main {
                 }
                 if (pregunta2 == 3) {
                     System.out.println(preguntas[2]);
-                    System.out.println("a. Steam");
-                    System.out.println("b. Epic Games Launcher");
-                    System.out.println("c. Ubisoft Connect");
+                    System.out.println("1. Steam");
+                    System.out.println("2. Epic Games Launcher");
+                    System.out.println("3. Ubisoft Connect");
 
                     respuesta3 = sc.nextInt();
 
@@ -95,9 +100,9 @@ public class Main {
                         System.out.println("Incorrecto. Pasemos con la ultima pregunta");
                     }
                     System.out.println(preguntas [1]);
-                    System.out.println("a. 2004");
-                    System.out.println("b. 2005");
-                    System.out.println("c. 2008");
+                    System.out.println("1. 2004");
+                    System.out.println("2. 2005");
+                    System.out.println("3. 2008");
 
                     respuesta2 = sc.nextInt();
 
@@ -117,7 +122,103 @@ public class Main {
                 } else {
                     System.out.println("0/3. Puedes intentarlo de nuevo");
                 }
+            }
 
+
+            if (pregunta == 2) {
+                System.out.println(preguntas[1]);
+
+                System.out.println("1. 2004");
+                System.out.println("2. 2005");
+                System.out.println("3. 2008");
+
+                respuesta2 = sc.nextInt();
+
+                if (respuesta2 == 1) {
+                    System.out.println("Correcto, seleccione siguiente pregunta");
+                } else {
+                    System.out.println("Incorrecto, seleccione la siguiente pregunta");
+                }
+
+                pregunta2 = sc.nextInt();
+
+                while (pregunta2 == 2) {
+                    System.out.println("Pregunta ya seleccionada, por favor seleccione otra");
+                    pregunta2 = sc.nextInt();
+                    if ((pregunta2 == 1) || pregunta2 == 3); {
+                        break;
+                    }
+                }
+                while ((pregunta2 < 1) || (pregunta2 > 3)) {
+                    System.out.println("Selección no válida, intentelo de nuevo");
+                    pregunta2 = sc.nextInt();
+                    if ((pregunta2 == 1) || (pregunta2 == 3));
+                }
+                if (pregunta2 == 3) {
+                    System.out.println(preguntas [2]);
+
+                    System.out.println("1. Steam");
+                    System.out.println("2. Epic Games Launcher");
+                    System.out.println("3. Ubisoft Connect");
+
+                    respuesta3 = sc.nextInt();
+
+                    if (respuesta3 == 1) {
+                        System.out.println("Correcto");
+                        System.out.println("Pasemos a la ultima pregunta");
+                    } else {
+                        System.out.println("Incorrecto, contiuemos con la ultima pregunta");
+                    }
+                    System.out.println(preguntas[0]);
+
+                    System.out.println("1. God of War Ragnarok");
+                    System.out.println("2. Elden ring");
+                    System.out.println("3. It Takes two");
+
+                    respuesta = sc.nextInt();
+
+                    if (respuesta == 1) {
+                        System.out.println("Correcto! Has finalizado el cuestionario, ahora pasaremos al recuento de tus preguntas correctas");
+                    } else {
+                        System.out.println("Incorrecto. Ahora pasemos al recuento de preguntas correctas");
+                    }
+                }
+                if (pregunta2 == 1) {
+                    System.out.println(preguntas[0]);
+
+                    System.out.println("1. God of War Ragnarok");
+                    System.out.println("2. Elden ring");
+                    System.out.println("3. It Takes two");
+
+                    respuesta = sc.nextInt();
+
+                    if (respuesta == 2) {
+                        System.out.println("Correcto! Pasemos con la ultima pregunta");
+                    } else {
+                        System.out.println("Incorrecto. Pasemos con la ultima pregunta");
+                    }
+                    System.out.println(preguntas [2]);
+
+                    System.out.println("1. Steam");
+                    System.out.println("2. Epic Games Launcher");
+                    System.out.println("3. Ubisoft Connect");
+
+                    respuesta3 = sc.nextInt();
+
+                    if (respuesta3 == 1) {
+                        System.out.println("Correcto. Ahora continuemos con el recuento de preguntas acertadas.");
+                    } else {
+                        System.out.println("Incorrecto. Continuemos con el recuento de preguntas");
+                    }
+                } if (respuesta == 2 && respuesta2 == 1 && respuesta3 == 1) {
+                    System.out.println("3/3. Enhorabuena, tienes concocimientos básicos sobre los videojuegos!");
+                } else if (((respuesta == 2) && (respuesta2 == 1) || ((respuesta == 2) && (respuesta3 == 1)) || ((respuesta2 == 1) && (respuesta3 == 1)))) {
+                    System.out.println("2/3. Enhorabuena, has acertado 2 de 3, sigue así!");
+                } else if ((respuesta == 2) || (respuesta2 == 1) || (respuesta3 == 1)) {
+                    System.out.println("1/3. Puedes seguir mejorando");
+                } else {
+                    System.out.println("0/3. Puedes intentarlo de nuevo");
+                }
             }
         }
 
